5babd361363151cf28b1f80bdea24f9d5efba5b2
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-18 15:34:52 -0300

    create django, templates, urls, views updates

---
+++ recipes/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Wanderson Rossetti',
+    })
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
