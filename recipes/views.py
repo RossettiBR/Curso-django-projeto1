3a529e91d3bad08a75652afddac9c0397b15f5ea
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-19 20:46:14 -0300

    create models, update urls, categorys

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
@@ -13,4 +22,6 @@ def recipe(request, id):
         'recipe': make_recipe(),
         'is_detail_page': True,
     })
+
+
 
