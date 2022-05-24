878f7d2d5d6f6cbdc800c4d3f8420b901bb7bf38
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-24 16:36:41 -0300

    create search.html refactor test_views

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -11,8 +11,14 @@ class RecipeURLsTest(TestCase):
 
     def test_recipes_category_url_is_correct(self):
         url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1')
+        self.assertEqual(url, '/recipes/category/1/')
 
     def test_recipes_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+    def test_recipe_search_url_is_correct(self):
+        url = reverse('recipes:search')
+        self.assertEqual(url, '/recipes/search/')
+
+# RED - GREEN - REFACTOR
