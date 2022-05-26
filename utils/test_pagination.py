c48d4303e9535c2d5d0094d98044249b3042fa3d
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-26 20:28:50 -0300

    update

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -6,3 +6,6 @@ class PaginationTest(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range()
         self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_first_range_is_static_if_current_is_less_than_middle_page(self):
+        
