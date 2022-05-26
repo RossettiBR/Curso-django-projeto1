9fe7720b28b3a18c38ed97e24682682e01afb55b
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-25 21:52:03 -0300

    create test_recipe_model update models

--- recipes/models.py
+++ recipes/models.py
@@ -22,8 +22,8 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')  # noqa:E501
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)  # noqa:E501
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
