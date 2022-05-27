bb14cb3188be11f6330e9c4e5a27b4250d2c3fe5
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-27 19:25:36 -0300

    create constance PER_PAGES

--- recipes/views.py
+++ recipes/views.py
@@ -5,12 +5,14 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.pagination import make_pagination
 from recipes.models import Recipe
 
+PER_PAGES = 9
+
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -26,7 +28,7 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -58,7 +60,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" ',
