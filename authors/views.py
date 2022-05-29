3fd12dc1e9df897ba385e2d54b869ddd69c2b644
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-29 02:35:14 -0300

    def  register_create views.py, register update

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.http import Http404
 from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm
+    form = RegisterForm()
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
+    })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+        
+    form = RegisterForm(request.POST)
+    return render(request, 'author/pages/register_view.html', {
+        'form': form
     })
