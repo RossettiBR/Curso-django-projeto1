72f733eac21147c39f0e56007a3f4ac8523d0096
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-29 02:19:41 -0300

    create forms in register_view.html, update views

--- authors/forms.py
+++ authors/forms.py
@@ -3,4 +3,12 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(forms.ModelForm):
-    ...
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
