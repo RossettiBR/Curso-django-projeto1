a1ab6d85347c4897af3531bb90a5e278f84f29d9
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-30 03:55:56 -0300

    update

--- authors/forms.py
+++ authors/forms.py
@@ -96,7 +96,19 @@ class RegisterForm(forms.ModelForm):
             'last_name',
             'username',
             'email',
+            'password',
         ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid'
+            )
+        
+        return email
 
     def clean(self):
         cleaned_data = super().clean()
