25bf03ad6715801e2e83877085d9a9dc338ac112
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-29 23:03:58 -0300

    update, create test_author

---
+++ authors/tests/test_author_register_form.py
@@ -0,0 +1,10 @@
+from authors.forms import RegisterForm
+from django.test import TestCase
+
+
+class AuthorReisterFormUnitTest(TestCase):
+    def test_first_name_placeholder_is_correct(self):
+        form = RegisterForm()
+        placeholder = form['first_name'].field.widget.attrs['placeholder']
+
+        self.assertEqual('Ex.:John', placeholder)
--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import Http404
+from django.shortcuts import redirect, render
+
 from .forms import RegisterForm
 
 
@@ -18,5 +20,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del(request.session['register_form_data'])
 
     return redirect('authors:register')
