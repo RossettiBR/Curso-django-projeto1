15eb18b6d197da2a46452f2c550826406b7cb2f1
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-30 00:05:38 -0300

    forms refactor,  update test_author

--- authors/forms.py
+++ authors/forms.py
@@ -18,12 +18,12 @@ def strong_password(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
 
     if not regex.match(password):
-        raise ValidationError(('Password must have at least one uppercase letter, '   # noqa:E501
-                               'one lowercase letter and one number. The length shold be '  # noqa:E501
-                               'at least 8 characters.'
-                               ),
-                              code='Invalid'
-                              )
+        raise ValidationError(
+            ('Password must have at least one uppercase letter, '
+                'one lowercase letter and one number. The length shold be '
+                'at least 8 characters.'),
+            code='Invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
@@ -33,25 +33,28 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
 
     password = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Your password'
-        }),
-
+        widget=forms.PasswordInput(),
         error_messages={
-            'required': 'Password must not be emoty'
+            'required': 'Password must not be empty'
         },
-
-        validators=[strong_password]
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length shold be '
+            'at least 8 characters.'
+        ),
+        validators=[strong_password],
+        label='Password'
 
     )
     password2 = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Repeat your password'
-        })
+        widget=forms.PasswordInput(),
+        label='Password2'
     )
 
     class Meta:
@@ -61,7 +64,6 @@ class RegisterForm(forms.ModelForm):
             'last_name',
             'username',
             'email',
-            'password',
         ]
 
         labels = {
@@ -69,7 +71,6 @@ class RegisterForm(forms.ModelForm):
             'last_name': 'Last name',
             'username': 'Username',
             'email': 'E-mail',
-            'password': 'Password',
         }
 
         help_texts = {
@@ -80,16 +81,6 @@ class RegisterForm(forms.ModelForm):
             'username': {
                 'required': 'This field must not be empty',
             }
-        }
-
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Type first name here',
-                'class': 'input text-input',
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Type your password herer',
-            })
         }
 
     def clean_password(self):
--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,10 +1,48 @@
 from authors.forms import RegisterForm
 from django.test import TestCase
+from parameterized import parameterized
 
 
 class AuthorReisterFormUnitTest(TestCase):
-    def test_first_name_placeholder_is_correct(self):
+    @parameterized.expand([
+        ('username', 'Your username'),
+        ('email', 'Your e-mail'),
+        ('first_name', 'Ex.: John'),
+        ('last_name', 'Ex.: Doe'),
+        ('password', 'Type your password'),
+        ('password2', 'Repeat your password'),
+    ])
+    def test_fields_placeholder_is_correct(self, field, placeholder):
         form = RegisterForm()
-        placeholder = form['first_name'].field.widget.attrs['placeholder']
+        current_placeholder = form[field].field.widget.attrs['placeholder']
 
-        self.assertEqual('Ex.:John', placeholder)
+        self.assertEqual(current_placeholder, placeholder)
+
+    @parameterized.expand([
+        ('username', (
+            'Obrigatório. 150 caracteres ou menos. '
+            'Letras, números e @/./+/-/_ apenas.')),
+        ('email', 'The e-mail must be valid.'),
+        ('password', (
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length shold be '
+            'at least 8 characters.'
+        )),
+    ])
+    def test_fields_help_text(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.help_text
+        self.assertEqual(current, needed)
+
+    @parameterized.expand([
+        ('first_name', 'First name'),
+        ('last_name', 'Last name'),
+        ('username', 'Username'),
+        ('email', 'E-mail'),
+        ('password', 'Password'),
+        ('password2', 'Password2')
+    ])
+    def test_fields_label(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.label
+        self.assertEqual(current, needed)
