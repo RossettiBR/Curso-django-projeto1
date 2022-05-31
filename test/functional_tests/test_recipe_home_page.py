be052d27d3d134a347e8f8a14c8883d57b7020aa
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-31 02:06:44 -0300

    refactor

--- test/functional_tests/test_recipe_home_page.py
+++ test/functional_tests/test_recipe_home_page.py
@@ -19,7 +19,7 @@ class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
 
 
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    def test_the_test(self):
+    def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here', body.text)
