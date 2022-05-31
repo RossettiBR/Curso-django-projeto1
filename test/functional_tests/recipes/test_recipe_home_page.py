6c65e4a6cc2e933783d8161b2e70e31c61355dc6
Wanderson Rossetti <pastarossetti@gmail.com>
2022-05-31 02:19:52 -0300

    create __init__, update test

--- test/functional_tests/recipes/test_recipe_home_page.py
+++ test/functional_tests/recipes/test_recipe_home_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from base import RecipeBaseFunctionalTest
+from . base import RecipeBaseFunctionalTest
 
 
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
