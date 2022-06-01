1676b34b67a96df05ba9d577e519c07c7eed932f
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-01 01:24:49 -0300

    add Accessibility pagination

--- test/functional_tests/recipes/test_recipe_home_page.py
+++ test/functional_tests/recipes/test_recipe_home_page.py
@@ -1,3 +1,4 @@
+
 import pytest
 from unittest.mock import patch
 from selenium.webdriver.common.by import By
@@ -8,13 +9,10 @@ from . base import RecipeBaseFunctionalTest
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
-        self.make_recipe_in_batch(qtd=20)
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.sleep()
-        #self.assertIn('No recipes found here', body.text)
+        self.assertIn('No recipes found here', body.text)
 
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_search_input_can_find_correct_recipes(self):
@@ -44,3 +42,12 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
             title_needed,
             self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
         )
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        recipes = self.make_recipe_in_batch()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        self.sleep(10)
