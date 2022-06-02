f9332762f24923718d61e32c81f48a2cf8e0b064
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-02 03:27:02 -0300

    create defs, refacor, update

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from recipes.models import Recipe
+from .forms.recipe_form import AuthorRecipeForm
 from .forms import RegisterForm, LoginForm
 
 
@@ -84,5 +85,32 @@ def logout_view(request):
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard(request):
-    return render(request, 'authors/pages/dashboard.html')
+def dashboard(request,):
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+    return render(request, 'authors/pages/dashboard.html', context={
+        'recipes': recipes,
+    })
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_edit(request, id):
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    ).first()
+
+    if not recipe:
+        raise Http404()
+
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        instance=recipe
+    )
+
+    return render(request, 'authors/pages/dashboard_recipe.html', context={
+        'form': form
+    })
