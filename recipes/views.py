0c8802dca761d505d229fe8b08ff75cf1458e2ad
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-03 04:25:31 -0300

    refactor

--- recipes/views.py
+++ recipes/views.py
@@ -4,11 +4,38 @@ from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.pagination import make_pagination
-
+from django.views.generic import ListView
 from recipes.models import Recipe
 
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    paginate_by = None
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True,
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            context.get('recipes'),
+            PER_PAGE
+        )
+        context.update(
+            {'recipes': page_obj, 'pagination_range': pagination_range}
+        )
+        return context
 
 
 def home(request):
