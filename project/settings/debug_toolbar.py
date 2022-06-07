d1964a4b8fe39c3376b6d31b34c5b5be27f81ade
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-07 16:08:54 -0300

    refactor

--- project/settings/debug_toolbar.py
+++ project/settings/debug_toolbar.py
@@ -7,3 +7,7 @@ INSTALLED_APPS += ['debug_toolbar', ]
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ] + MIDDLEWARE
+# Django Debug ToolBar
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
