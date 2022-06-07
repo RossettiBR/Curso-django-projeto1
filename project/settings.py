c931d47c229c94620014626590e685463fa7be87
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-07 15:15:55 -0300

    FIx ALLOWED_HOSTS and CSRF_THUSTED_ORIGINS

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+from utils.enviroment import get_env_variable, parse_comma_sep_str_to_list
 from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 
-ALLOWED_HOSTS = []  # type: ignore
+ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('ALLOWED_HOSTS'))
+CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('CSRF_THUSTED_ORIGINS'))
 
 
 # Application definition
