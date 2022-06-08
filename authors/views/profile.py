8c84e8fb2b2892ce66279b93b76b72c479fbe6dc
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-07 23:39:19 -0300

    update, create url, views , html -> profile

---
+++ authors/views/profile.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+
+
+class ProfileView(TemplateView):
+    template_name = 'authors/pages/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
