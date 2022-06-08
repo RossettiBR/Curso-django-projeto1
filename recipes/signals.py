63a27e053caefeba3fac7e470a1925a83b9cfb1b
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-08 14:18:54 -0300

    update

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,8 +1,17 @@
-from django.db.models.signals import post_save
+import os
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from recipes.models import Recipe
 
 
-@receiver(post_save, sender=Recipe)
-def recipe_post_signal(sender, instance, created, *args, **kwargs):
-    ....
+def delete_cover(instance):
+    try:
+        os.remove(instance.cover.path)
+    except (ValueError, FileNotFoundError):
+        ...
+
+
+@receiver(pre_delete, sender=Recipe)
+def recipe_post_signal(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.get(pk=instance.pk)
+    delete_cover(old_instance)
