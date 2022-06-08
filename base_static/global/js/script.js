b899cec0590e18fc5b1bf33a8c14d9ddd682d06e
Wanderson Rossetti <pastarossetti@gmail.com>
2022-06-07 22:56:15 -0300

    update

--- base_static/global/js/script.js
+++ base_static/global/js/script.js
@@ -44,5 +44,13 @@
 })();
 
 (() => {
-    
+    const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
+    const formLogout = document.querySelector('.form-logout');
+
+    for (const link of authorsLogoutLinks) {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            formLogout.submit();
+        });
+    }
 })();
